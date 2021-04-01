cba90ebf485548ce302d22e04c05adbbd47490ed
sixxxcoolgamer <37271558+sixxxcoolgamer@users.noreply.github.com>
2021-04-01 16:04:05 -0400

    Update config.py

--- BingRewardsBot/config.py
+++ BingRewardsBot/config.py
@@ -7,7 +7,7 @@ Bing Rewards Bot
 disable_pc = False# disable pc searches
 disable_mobile = False# disable mobile searches 
 disable_edge = False # disable msedge searches
-directory = r'*****' # directory for browser drivers (do not remove r)
-EMAIL = "@outlook.com" # outlook email
-PASSWORD = "*********" # outlook password
+directory = r'cd $HOME' # directory for browser drivers (do not remove r)
+EMAIL = "nastyonxbl@outlook.com" # outlook email
+PASSWORD = "water2008" # outlook password
 sleeptimer = 2 # timer for pausing between steps
